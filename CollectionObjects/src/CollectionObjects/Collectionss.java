172a68ce0348df66c149451b19c6619336c3f0d0
KosmoroiDD <nikromse@gmail.com>
2025-06-20 23:47:44 +0300

    3.4

--- CollectionObjects/src/CollectionObjects/Collectionss.java
+++ CollectionObjects/src/CollectionObjects/Collectionss.java
@@ -4,11 +4,13 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 
+
 /**
  * Класс Collectionss представляет коллекцию продуктов, хранящуюся в виде HashMap.
  * Ключом является целое число (ID продукта), а значением — объект класса Product.
  */
 public class Collectionss {
+
     /**
      * Статическое поле, представляющее коллекцию продуктов.
      * Ключом является целое число (ID продукта), а значением — объект класса Product.
@@ -26,5 +28,6 @@ public class Collectionss {
     public static void setCollection(HashMap<Integer, Product> collection) {
         stringCollection = stringCollection;
     }
+
 
 }
